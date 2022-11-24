72a3d99bd45a6e6c6a3595323a3e163a031c8a5a
Venla <venlaj@hotmail.com>
2022-11-24 17:02:49 +0200

    Code cleaning

--- frontend/src/components/UpdateExisting.js
+++ frontend/src/components/UpdateExisting.js
@@ -14,28 +14,56 @@ const UpdateExisting = ({book, onUpdate }) => {
 
   return (
     <>
-    <form className="form" onSubmit={onSubmit}>
-    <div className="form-container">
-        <label>Description</label>
-        <textarea key={book._id} className="form-input-description" type="text" placeholder={book.description}></textarea>
-      </div>
-      <h3 className='h3'>Update the book {book.title}</h3>
-      <div className="form-container">
-        <label>Title</label>
-        <input key={book._id} className="form-input" type="text" value={title} onChange={(e) => setTitle(e.target.value)}></input>
-      </div>
-      <div className="form-container">
-        <label>Author</label>
-        <input key={book._id} className="form-input" type="text" value={author} onChange={(e) => setAuthor(e.target.value)}></input>
-      </div>
-      <div className="form-container">
-        <label>Description</label>
-        <input key={book._id} className="form-input" type="text" value={description} onChange={(e) => setDescription(e.target.value)}></input>
-      </div>
-      <input className="btn btn-block" type="Submit" defaultValue="Update book" onClick={() => onUpdate(book._id, title, author, description)}></input>
-    </form>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-container">
+          <label>Description</label>
+          <textarea
+            key={book._id}
+            className="form-input-description"
+            type="text"
+            placeholder={book.description}
+          ></textarea>
+        </div>
+        <h3 className="h3">Update the book {book.title}</h3>
+        <div className="form-container">
+          <label>Title</label>
+          <input
+            key={book._id}
+            className="form-input"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+        </div>
+        <div className="form-container">
+          <label>Author</label>
+          <input
+            key={book._id}
+            className="form-input"
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          ></input>
+        </div>
+        <div className="form-container">
+          <label>Description</label>
+          <input
+            key={book._id}
+            className="form-input"
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></input>
+        </div>
+        <input
+          className="btn btn-block"
+          type="Submit"
+          defaultValue="Update book"
+          onClick={() => onUpdate(book._id, title, author, description)}
+        ></input>
+      </form>
     </>
-  )
+  );
 }
 
 export default UpdateExisting
